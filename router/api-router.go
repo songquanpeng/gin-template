abcbd0be208afdc919d52fda265d324cadb962ed
JustSong <quanpengsong@gmail.com>
2022-10-30 16:52:16 +0800

    refactor: update project structure

--- router/api-router.go
+++ router/api-router.go
@@ -1,4 +1,4 @@
-package v0
+package router
 
 import (
 	"gin-template/controller"
--- router/main.go
+++ router/main.go
@@ -2,11 +2,10 @@ package router
 
 import (
 	"embed"
-	"gin-template/router/v0"
 	"github.com/gin-gonic/gin"
 )
 
 func SetRouter(router *gin.Engine, buildFS embed.FS, indexPage []byte) {
-	v0.SetApiRouter(router)
+	SetApiRouter(router)
 	setWebRouter(router, buildFS, indexPage)
 }
