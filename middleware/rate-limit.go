194d7546b0586f73ee63ee66e002eb2cc0f8714f
JustSong <quanpengsong@gmail.com>
2022-10-30 15:21:31 +0800

    fix: fix Redis based rate limiter

--- middleware/rate-limit.go
+++ middleware/rate-limit.go
@@ -11,26 +11,38 @@ import (
 
 var timeFormat = "2006-01-02T15:04:05.000Z"
 
-func rateLimitHelper(c *gin.Context, maxRequestPerMinute int, mark string) {
+var keyExpirationDuration = 10 * time.Minute
+
+func redisRateLimiter(c *gin.Context, maxRequestPerMinute int, mark string) {
 	ctx := context.Background()
 	rdb := common.RDB
 	key := "rateLimit:" + mark + c.ClientIP()
 	listLength, err := rdb.LLen(ctx, key).Result()
 	if err != nil {
 		fmt.Println(err.Error())
+		c.Status(http.StatusInternalServerError)
+		c.Abort()
+		return
 	}
 	if listLength < int64(maxRequestPerMinute) {
 		rdb.LPush(ctx, key, time.Now().Format(timeFormat))
+		rdb.Expire(ctx, key, keyExpirationDuration)
 	} else {
 		oldTimeStr, _ := rdb.LIndex(ctx, key, -1).Result()
 		oldTime, err := time.Parse(timeFormat, oldTimeStr)
 		if err != nil {
 			fmt.Println(err)
+			c.Status(http.StatusInternalServerError)
+			c.Abort()
+			return
 		}
 		newTimeStr := time.Now().Format(timeFormat)
 		newTime, err := time.Parse(timeFormat, newTimeStr)
 		if err != nil {
 			fmt.Println(err)
+			c.Status(http.StatusInternalServerError)
+			c.Abort()
+			return
 		}
 		// time.Since will return negative number!
 		// See: https://stackoverflow.com/questions/50970900/why-is-time-since-returning-negative-durations-on-windows
@@ -41,47 +53,40 @@ func rateLimitHelper(c *gin.Context, maxRequestPerMinute int, mark string) {
 		} else {
 			rdb.LPush(ctx, key, time.Now().Format(timeFormat))
 			rdb.LTrim(ctx, key, 0, int64(maxRequestPerMinute-1))
+			rdb.Expire(ctx, key, keyExpirationDuration)
+		}
+	}
+}
+
+func memoryRateLimiter(c *gin.Context, maxRequestPerMinute int, mark string) {
+	// TODO: memoryRateLimiter
+	c.Next()
+}
+
+func rateLimitFactory(maxRequestPerMinute int, mark string) func(c *gin.Context) {
+	if common.RedisEnabled {
+		return func(c *gin.Context) {
+			redisRateLimiter(c, maxRequestPerMinute, mark)
+		}
+	} else {
+		return func(c *gin.Context) {
+			c.Next()
 		}
 	}
 }
 
 func GlobalWebRateLimit() func(c *gin.Context) {
-	return func(c *gin.Context) {
-		if common.RedisEnabled {
-			rateLimitHelper(c, common.GlobalWebRateLimit, "GW")
-		} else {
-			c.Next()
-		}
-	}
+	return rateLimitFactory(common.GlobalWebRateLimit, "GW")
 }
 
 func GlobalAPIRateLimit() func(c *gin.Context) {
-	return func(c *gin.Context) {
-		if common.RedisEnabled {
-			rateLimitHelper(c, common.GlobalApiRateLimit, "GA")
-		} else {
-			c.Next()
-		}
-	}
+	return rateLimitFactory(common.GlobalApiRateLimit, "GA")
 }
 
 func CriticalRateLimit() func(c *gin.Context) {
-	return func(c *gin.Context) {
-		if common.RedisEnabled {
-			rateLimitHelper(c, common.CriticalRateLimit, "CT")
-		} else {
-			c.Next()
-		}
-
-	}
+	return rateLimitFactory(common.CriticalRateLimit, "CT")
 }
 
 func DownloadRateLimit() func(c *gin.Context) {
-	return func(c *gin.Context) {
-		if common.RedisEnabled {
-			rateLimitHelper(c, common.DownloadRateLimit, "CM")
-		} else {
-			c.Next()
-		}
-	}
+	return rateLimitFactory(common.DownloadRateLimit, "CM")
 }
